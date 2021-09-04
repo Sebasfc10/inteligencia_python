45a6b7ad33b800992dbab4afbef969910c0c1b14
Sebasfc10 <79613872+Sebasfc10@users.noreply.github.com>
2021-09-04 15:30:39 -0500

    ejercicio 1 - calcular masa

--- ejer.py
+++ ejer.py
@@ -11,3 +11,14 @@ n = ((((8+2-4)**2)*5+8+7/2 -30*5) / 2*5-3)**5 + 15 *3 - 9/3
 m = ((z)**2)*3+n
 y = (((((z+2-n)**2)*m+8/2-30)/2*5-3)**5+15*3-9/3)**2-5/4
 print(y)
+
+#ejercicios con solucion a la problematica resuelta en python
+"""
+1 - Haga un algoritmo que calcule la masa de la siguiente fórmula:
+Masa = (presión * volúmen) / (0.37 * (temperatura + 460))
+"""
+presion = float(input('ingrese la presion: '))
+volumen = float(input('ingrese la volumen: '))
+temperatura = float(input('ingrese la temperatura: '))
+masa = (presion * volumen)/(0.37 * (temperatura + 460))
+print(f'esta es la masa calculada: {masa}')
